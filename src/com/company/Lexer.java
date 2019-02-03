656ff5f015a307fbb733a154c00f66ff3700452f
deyandanov <37149777+deyandanov@users.noreply.github.com>
2019-02-03 21:44:04 +0100

    Lexer Test Added

--- src/com/company/Lexer.java
+++ src/com/company/Lexer.java
@@ -1,4 +1,31 @@
 package com.company;
 
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 public class Lexer {
+
+    private SortedMap<DFAState, Map<Character, DFAState>> stateTransitionTable = new TreeMap<DFAState, Map<Character, DFAState>>();
+    public Lexer(SortedMap<DFAState, Map<Character, DFAState>> stateTransitionTable){
+        this.stateTransitionTable = stateTransitionTable;
+    }
+    public boolean match(String string){
+
+        char[] charArray = string.toCharArray();
+        DFAState state = stateTransitionTable.firstKey();
+
+        for (char symbol:charArray) {
+            char letter = symbol;
+
+            if (state.isAcceptingState== true){
+                return true;
+            }
+            if (stateTransitionTable.get(state).containsKey(letter)){
+                state = stateTransitionTable.get(state).get(letter);
+            }
+            else return false;
+        }
+        return false;
+    }
 }
---
+++ src/com/company/LexerTest.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class LexerTest {
+
+    @Test
+    public void match() {
+        SortedMap<DFAState, Map<Character, DFAState>> stateTransitionTable = new TreeMap<DFAState, Map<Character, DFAState>>();
+        Set<Integer> positions = new HashSet<Integer>();
+        Map<Character,DFAState> transmissionMap = new TreeMap<>();
+
+        positions.add(2);
+        DFAState S1 = new DFAState(1,false,positions);
+        DFAState S2 = new DFAState(2,false,positions);
+        DFAState S3 = new DFAState(3,true,positions);
+        DFAState S4 = new DFAState(4,false,positions);
+
+
+        stateTransitionTable.put(S3,transmissionMap);
+
+        transmissionMap = new TreeMap<>();
+        transmissionMap.put('a',S2);
+        stateTransitionTable.put(S1,transmissionMap);
+
+        transmissionMap = new TreeMap<>();
+        transmissionMap.put('b',S4);
+        stateTransitionTable.put(S2,transmissionMap);
+
+        transmissionMap = new TreeMap<>();
+        transmissionMap.put('a',S3);
+        stateTransitionTable.put(S4,transmissionMap);
+
+
+        Lexer lexer = new Lexer(stateTransitionTable);
+        Assert.assertTrue(lexer.match("ababbbaaa"));
+        Assert.assertFalse(lexer.match("abbaabb"));
+
+    }
+}
