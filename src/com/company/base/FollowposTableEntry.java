ba7c736316af8c2c76061496ddb405ba4a10612f
EricGend <34488142+EricGend@users.noreply.github.com>
2019-02-07 14:41:20 +0100

    added getter and setter
    attributs private as well

--- src/com/company/base/FollowposTableEntry.java
+++ src/com/company/base/FollowposTableEntry.java
@@ -4,9 +4,25 @@ import java.util.HashSet;
 import java.util.Set;
 
 class FollowposTableEntry {
-    public final int position;
-    public final String symbol;
-    public final Set<Integer> followpos = new HashSet<>();
+    private final int position;
+    private final String symbol;
+    private final Set<Integer> followpos = new HashSet<>();
+
+
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public Set<Integer> getFollowpos() {
+        return followpos;
+    }
+
+
 
     public FollowposTableEntry(int position, String symbol) {
         this.position = position;
