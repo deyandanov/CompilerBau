06832903b89d84a7391fb59fca27d8d33fa3e3fd
noahboerger <35099943+noahboerger@users.noreply.github.com>
2019-02-07 13:01:49 +0100

    Small Changes of Project Structure

--- src/com/company/LexerTest.java
+++ src/com/company/LexerTest.java
@@ -13,28 +13,28 @@ public class LexerTest {
     public void match() {
         SortedMap<DFAState, Map<Character, DFAState>> stateTransitionTable = new TreeMap<DFAState, Map<Character, DFAState>>();
         Set<Integer> positions = new HashSet<Integer>();
-        Map<Character,DFAState> transmissionMap = new TreeMap<>();
+        Map<Character, DFAState> transmissionMap = new TreeMap<>();
 
         positions.add(2);
-        DFAState S1 = new DFAState(1,false,positions);
-        DFAState S2 = new DFAState(2,false,positions);
-        DFAState S3 = new DFAState(3,true,positions);
-        DFAState S4 = new DFAState(4,false,positions);
+        DFAState S1 = new DFAState(1, false, positions);
+        DFAState S2 = new DFAState(2, false, positions);
+        DFAState S3 = new DFAState(3, true, positions);
+        DFAState S4 = new DFAState(4, false, positions);
 
 
-        stateTransitionTable.put(S3,transmissionMap);
-
-        transmissionMap = new TreeMap<>();
-        transmissionMap.put('a',S2);
-        stateTransitionTable.put(S1,transmissionMap);
+        stateTransitionTable.put(S3, transmissionMap);
 
         transmissionMap = new TreeMap<>();
-        transmissionMap.put('b',S4);
-        stateTransitionTable.put(S2,transmissionMap);
+        transmissionMap.put('a', S2);
+        stateTransitionTable.put(S1, transmissionMap);
 
         transmissionMap = new TreeMap<>();
-        transmissionMap.put('a',S3);
-        stateTransitionTable.put(S4,transmissionMap);
+        transmissionMap.put('b', S4);
+        stateTransitionTable.put(S2, transmissionMap);
+
+        transmissionMap = new TreeMap<>();
+        transmissionMap.put('a', S3);
+        stateTransitionTable.put(S4, transmissionMap);
 
 
         Lexer lexer = new Lexer(stateTransitionTable);
