1a7fd76d1380db400ca7f9ee292019e3db26aff4
noahboerger <35099943+noahboerger@users.noreply.github.com>
2019-02-10 18:31:11 +0100

    Main test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,45 @@ package com.company;
 import com.company.base.testres.TestUtilities;
 import com.company.base.Visitable;
 import com.company.base.Visitor;
+import com.company.parser.ITopDownParser;
 import com.company.parser.TopDownParser;
 import com.company.visitor.first.VisitorFirst;
 import com.company.visitor.second.FollowposTableEntry;
 import com.company.visitor.second.VisitorSecond;
 
+import java.util.Scanner;
 import java.util.SortedMap;
 
 public class Main {
 
     public static void main(String[] args) {
-        Visitable visitable = TestUtilities.createTestTree();
+
+        ITopDownParser parser = new TopDownParser();
+        Visitor visitorFirst = new VisitorFirst();
+        Visitor visitorSecond = new VisitorSecond();
+
+        Visitable visitable = null;
+
+        Scanner scanner = new Scanner(System.in);
+        String regEx = scanner.next();
+
+        visitable = parser.parse(regEx);
+
+        if(visitable == null) {
+            System.out.println("Ihr Ausdruck konnte nicht geparst werden!");
+            return;
+        }
+
+        visitable.accept(visitorFirst);
+        visitable.accept(visitorSecond);
+
+        SortedMap<Integer, FollowposTableEntry>  followposTableEntries =((VisitorSecond) visitorSecond).getFollowposTableEntries();
+
+        //TODO: Ausführen des DEA-Erzeugers und des Lexers
+
+
+
+        /*Visitable visitable = TestUtilities.createTestTree();
         Visitor visitorFirst = new VisitorFirst();
         visitorFirst.visit(visitable);
         System.out.println(visitable.isNullable());
@@ -37,6 +65,6 @@ public class Main {
             System.out.println("E");
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        }*/
     }
 }
--- src/com/company/parser/TopDownParser.java
+++ src/com/company/parser/TopDownParser.java
@@ -11,16 +11,15 @@ public class TopDownParser implements ITopDownParser {
     private String regEx;
 
     @Override
-    public Visitable parse (String regEx) throws Exception {
+    public Visitable parse(String regEx) {
         this.regEx = regEx;
         return start();
     }
 
-    private Visitable start () {
+    private Visitable start() {
         if (regEx.charAt(0) == '#') {
             return new OperandNode("#");
-        }
-        else if (regEx.charAt(0) == '(') {
+        } else if (regEx.charAt(0) == '(') {
             OperandNode leaf = new OperandNode("#");
             BinOpNode root = new BinOpNode("°", regExp(null, 1), leaf);
             return root;
@@ -31,29 +30,27 @@ public class TopDownParser implements ITopDownParser {
     private Visitable regExp(Visitable p, int pos) {
         char c = regEx.charAt(pos);
 
-        if (c >= '0' && c <= '9' || c >= 'A' && c <= 'Z' ||c >= 'a' && c <= 'z' || c == '(') {
-            return (re (term(null, pos + 1), pos + 1));
+        if (c >= '0' && c <= '9' || c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' || c == '(') {
+            return (re(term(null, pos + 1), pos + 1));
         }
 
         return null;
     }
 
-    private Visitable term (Visitable p, int pos) {
+    private Visitable term(Visitable p, int pos) {
         char c = regEx.charAt(pos);
 
-        if (c >= '0' && c <= '9' || c >= 'A' && c <= 'Z' ||c >= 'a' && c <= 'z' || c == '(') {
+        if (c >= '0' && c <= '9' || c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' || c == '(') {
             Visitable term1Parameter = null;
             if (p != null) {
                 //factor term
-                BinOpNode root = new BinOpNode("°", p, factor(null, pos +1));
+                BinOpNode root = new BinOpNode("°", p, factor(null, pos + 1));
                 term1Parameter = root;
-            }
-            else {
+            } else {
                 term1Parameter = factor(null, pos + 1);
             }
             return term(term1Parameter, pos + 1);
-        }
-        else if (c == '|' || c == ')') {
+        } else if (c == '|' || c == ')') {
             //epsilon
             return term(p, pos + 1);
         }
@@ -61,23 +58,22 @@ public class TopDownParser implements ITopDownParser {
         return null;
     }
 
-    private Visitable re (Visitable p, int pos) {
+    private Visitable re(Visitable p, int pos) {
 
         if (regEx.charAt(pos) == '|') {
             BinOpNode root = new BinOpNode("|", p, term(null, pos + 1));
             return re(root, pos + 1);
-        }
-        else if (regEx.charAt(pos) == ')') {
+        } else if (regEx.charAt(pos) == ')') {
             return re(p, pos + 1);
         }
 
         return null;
     }
 
-    private Visitable factor (Visitable p, int pos) {
+    private Visitable factor(Visitable p, int pos) {
         char c = regEx.charAt(pos);
 
-        if (c >= '0' && c <= '9' || c >= 'A' && c <= 'Z' ||c >= 'a' && c <= 'z' || c == '(') {
+        if (c >= '0' && c <= '9' || c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' || c == '(') {
             //elem hop
             return hop(elem(null, pos + 1), pos + 1);
         }
@@ -85,46 +81,42 @@ public class TopDownParser implements ITopDownParser {
         return null;
     }
 
-    private Visitable hop (Visitable p, int pos) {
+    private Visitable hop(Visitable p, int pos) {
         char c = regEx.charAt(pos);
 
-        if (c >= '0' && c <= '9' || c >= 'A' && c <= 'Z' ||c >= 'a' && c <= 'z' || c == '(' || c == '|' || c == ')') {
+        if (c >= '0' && c <= '9' || c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' || c == '(' || c == '|' || c == ')') {
             //epsilon
             return hop(p, pos + 1);
-        }
-        else if (c == '*') {
+        } else if (c == '*') {
             //sternchen
             return (new UnaryOpNode("*", p));
-        }
-        else if (c == '+') {
+        } else if (c == '+') {
             //plus
             return new UnaryOpNode("+", p);
-        }
-        else if (c == '?') {
+        } else if (c == '?') {
             //?
             return new UnaryOpNode("?", p);
         }
         return null;
     }
 
-    private Visitable elem (Visitable p, int pos) {
+    private Visitable elem(Visitable p, int pos) {
         char c = regEx.charAt(pos);
 
-        if (c >= '0' && c <= '9' || c >= 'A' && c <= 'Z' ||c >= 'a' && c <= 'z') {
+        if (c >= '0' && c <= '9' || c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z') {
             //alphanum
             return alphanum(null, pos + 1);
-        }
-        else if (c == '(') {
+        } else if (c == '(') {
             //klammer regexp klammer
             regExp(null, pos + 1);
         }
         return null;
     }
 
-    private Visitable alphanum (Visitable p, int pos) {
+    private Visitable alphanum(Visitable p, int pos) {
         char c = regEx.charAt(pos);
 
-        if (c >= '0' && c <= '9' || c >= 'A' && c <= 'Z' ||c >= 'a' && c <= 'z') {
+        if (c >= '0' && c <= '9' || c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z') {
             return new OperandNode("A");
         }
         return null;
