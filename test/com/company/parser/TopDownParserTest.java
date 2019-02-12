738cc0c68621052d589496bb2b5d5509936a4010
fpoh <fabianpohlink@gmx.de>
2019-02-12 13:38:55 +0100

    update

---
+++ test/com/company/parser/TopDownParserTest.java
@@ -0,0 +1,47 @@
+package com.company.parser;
+
+import com.company.base.exp.ExpressionNotValidException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class TopDownParserTest {
+
+    @Test
+    public void parse() {
+        ITopDownParser topDownParser = new TopDownParser();
+        ArrayList<String> wrongExp = new ArrayList<>();
+        ArrayList<String> correctExp = new ArrayList<>();
+
+        correctExp.add("(a|b)#");
+        correctExp.add("(88ddwdwdwdw|gegegeg*)#");
+        correctExp.add("(((a|b)*abb)+dwdwdw|wfwffwokf)#");
+
+        wrongExp.add("a|b");
+        wrongExp.add("a|b");
+
+        for (String exp : correctExp) {
+            try {
+                //Darf keine Exception werfen
+                topDownParser.parse(exp);
+                assertFalse(false);
+            } catch (ExpressionNotValidException e) {
+                assertFalse(true);
+            }
+        }
+
+        for (String exp : wrongExp) {
+            try {
+                //Muss Exception werfen
+                topDownParser.parse(exp);
+                assertFalse(true);
+            } catch (ExpressionNotValidException e) {
+                assertFalse(false);
+            }
+        }
+
+    }
+}
